a1f393ed2399e374d31fcbd924388b3ae6b22b7c
nabilihsani <73170434+nabilihsani@users.noreply.github.com>
2020-10-21 20:46:35 +0700

    Update db_login.php

--- Database/db_login.php
+++ Database/db_login.php
@@ -4,5 +4,8 @@ $db_host = 'localhost';
 $db_database = 'bukutamu';
 $db_username = 'root';
 $db_password = '';
-
+$db = new mysqli($db_host, $db_username, $db_password, $db_database);
+if ($db->connect_errno) {
+  die ("Could not connect to the database: <br />"). $db->connect_errno;
+}
 ?>
