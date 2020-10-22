a4568188021d4e80e9899a80cba5c78dc68115e0
nabilihsani <73170434+nabilihsani@users.noreply.github.com>
2020-10-22 18:07:32 +0700

    Update card.php

--- Database/card.php
+++ Database/card.php
@@ -1,17 +1,18 @@
 <?php 
-	require 'db_login.php';
-
-    
+    include 'db_login.php';
     $query = "SELECT * FROM kartu";
     $result = $db->query($query);
     $numRow = $result->num_rows;
     while ($row = mysqli_fetch_array($result)) {
-        print_r('</tr>');
-        print_r('<td>'.$row['Code'].'</td>');
-    	print_r('<td>'.$row['id'].'</td>');
-        print_r('<td>'.$row['status'].'</td>');
-    	print_r('</tr>');
+ ?>
+<?=
+    '</tr>
+    <td>'.$row['Code'].'</td>
+    <td>'.$row['id'].'</td>
+    <td>'.$row['status'].'</td>
+    </tr>'
+ ?>
+<?php 
     }
-
     $db->close();
  ?>
