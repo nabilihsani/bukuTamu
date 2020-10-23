cd77cfc16c669817eb61bba946b9d2a5b33347ec
nabilihsani <73170434+nabilihsani@users.noreply.github.com>
2020-10-23 12:55:06 +0700

    Update buttons.jqueryui.js

--- Buttons-1.6.1/js/buttons.jqueryui.js
+++ Buttons-1.6.1/js/buttons.jqueryui.js
@@ -3,13 +3,13 @@
  */
 
 (function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
+	if ( typeof define === "function" && define.amd ) {
 		// AMD
-		define( ['jquery', 'datatables.net-jqui', 'datatables.net-buttons'], function ( $ ) {
+		define( ["jquery", "datatables.net-jqui", "datatables.net-buttons"], function ( $ ) {
 			return factory( $, window, document );
 		} );
 	}
-	else if ( typeof exports === 'object' ) {
+	else if ( typeof exports === "object" ) {
 		// CommonJS
 		module.exports = function (root, $) {
 			if ( ! root ) {
@@ -17,11 +17,11 @@
 			}
 
 			if ( ! $ || ! $.fn.dataTable ) {
-				$ = require('datatables.net-jqui')(root, $).$;
+				$ = require("datatables.net-jqui")(root, $).$;
 			}
 
 			if ( ! $.fn.dataTable.Buttons ) {
-				require('datatables.net-buttons')(root, $);
+				require("datatables.net-buttons")(root, $);
 			}
 
 			return factory( $, root, root.document );
@@ -32,29 +32,29 @@
 		factory( jQuery, window, document );
 	}
 }(function( $, window, document, undefined ) {
-'use strict';
+"use strict";
 var DataTable = $.fn.dataTable;
 
 
 $.extend( true, DataTable.Buttons.defaults, {
 	dom: {
 		container: {
-			className: 'dt-buttons ui-buttonset'
+			className: "dt-buttons ui-buttonset"
 		},
 		button: {
-			className: 'dt-button ui-button ui-state-default ui-button-text-only',
-			disabled: 'ui-state-disabled',
-			active: 'ui-state-active'
+			className: "dt-button ui-button ui-state-default ui-button-text-only",
+			disabled: "ui-state-disabled",
+			active: "ui-state-active"
 		},
 		buttonLiner: {
-			tag: 'span',
-			className: 'ui-button-text'
+			tag: "span",
+			className: "ui-button-text"
 		}
 	}
 } );
 
 DataTable.ext.buttons.collection.text = function ( dt ) {
-	return dt.i18n('buttons.collection', 'Collection <span class="ui-button-icon-primary ui-icon ui-icon-triangle-1-s"/>');
+	return dt.i18n("buttons.collection", "Collection <span class="ui-button-icon-primary ui-icon ui-icon-triangle-1-s"/>");
 };
 
 
